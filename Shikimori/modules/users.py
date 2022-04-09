0e7a9a09b9a981f4a296deaedeea380c52128965
SOME1HING <yashprakash2005@gmail.com>
2022-04-09 19:41:00 +0530

    Update users.py

--- Shikimori/modules/users.py
+++ Shikimori/modules/users.py
@@ -178,8 +178,8 @@ __help__ = ""  # no help string
 BROADCAST_HANDLER = CommandHandler(
     ["broadcastall", "broadcastusers", "broadcastgroups"], broadcast
 , run_async = True)
-USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user, run_async = True)
-CHAT_CHECKER_HANDLER = MessageHandler(Filters.all & Filters.group, chat_checker, run_async = True)
+USER_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, log_user, run_async = True)
+CHAT_CHECKER_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, chat_checker, run_async = True)
 CHATLIST_HANDLER = CommandHandler("groups", chats, run_async = True)
 
 dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
