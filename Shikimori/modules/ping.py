556388ed53b01245cb85fba8d3a496b55e4f94ea
SOME1HING <yashprakash2005@gmail.com>
2022-04-10 00:38:36 +0530

    Update ping.py

--- Shikimori/modules/ping.py
+++ Shikimori/modules/ping.py
@@ -31,6 +31,35 @@ from Shikimori import dispatcher, OWNER_ID
 from Shikimori.modules.helper_funcs.alternate import typing_action
 from Shikimori.modules.helper_funcs.filters import CustomFilters
 
+StartTime = time.time()
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
 
 @typing_action
 def ping(update: Update, _):
@@ -39,10 +68,11 @@ def ping(update: Update, _):
     message = msg.reply_text("Pinging...")
     end_time = time.time()
     ping_time = round((end_time - start_time) * 1000, 3)
+    uptime = get_readable_time((time.time() - StartTime))
     message.edit_text(
         "<b>PONG</b> ✨\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>Service Uptime:</b> <code>{}</code>".format(ping_time, uptime),
         parse_mode=ParseMode.HTML,
     )
 
