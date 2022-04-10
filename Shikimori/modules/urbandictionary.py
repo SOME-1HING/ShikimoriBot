4e486cbf6f44c8e8e9aa2b33ded68f2a2ec61abe
SOME1HING <yashprakash2005@gmail.com>
2022-04-11 03:04:56 +0530

    Update urbandictionary.py

--- Shikimori/modules/urbandictionary.py
+++ Shikimori/modules/urbandictionary.py
@@ -24,3 +24,9 @@ dispatcher.add_handler(UD_HANDLER)
 
 __command_list__ = ["ud"]
 __handlers__ = [UD_HANDLER]
+
+__mod_name__ = "Urban Dictionary"
+__help__ = """
+*Urban Dictionary*
+ ❍ `/ud` : Search for word in Urban Dictionary
+"""
