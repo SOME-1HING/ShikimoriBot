6c23c9d2d4b49d8e294b4501e85411fa82a7680d
SOME1HING <yashprakash2005@gmail.com>
2022-04-10 23:06:31 +0530

    Update mongo.py

--- Shikimori/mongo.py
+++ Shikimori/mongo.py
@@ -3,9 +3,9 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from Shikimori import MONGO_DB_URI
-from Shikimori.confing import get_int_key, get_str_key
-from Shikimori.utils.logger import log
+from Senku import MONGO_DB_URI
+from Senku.confing import get_int_key, get_str_key
+from Senku.utils.logger import log
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_URI")
@@ -16,7 +16,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["Shikimorirobot"]
+db = client["senkurobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
