ff5960fc03a6dc227422cd2908080fe9f6bb7ed6
SOME1HING <yashprakash2005@gmail.com>
2022-04-11 02:58:21 +0530

    Update shippering.py

--- Shikimori/modules/shippering.py
+++ Shikimori/modules/shippering.py
@@ -74,4 +74,8 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
         await message.reply_text(e)
 
 
-__mod_name__ = "Couples"
+__mod_name__ = "Couples / Shippering"
+__help__ = """
+*Couples / Shippering*
+ ❍ `/couples` :  get couples of the day!
+"""
