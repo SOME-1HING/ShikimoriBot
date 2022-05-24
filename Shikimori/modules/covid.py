fb82132ce9ab4640d37a545ead28155b133e7394
Yash <yashprakash2005@gmail.com>
2022-05-25 05:07:21 +0530

    hmm

---
+++ Shikimori/modules/covid.py
@@ -0,0 +1,31 @@
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+from Shikimori import dispatcher
+from Shikimori.modules.disable import DisableAbleCommandHandler
+
+
+
+def covid(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text.split(" ", 1)
+    if len(text) == 1:
+        r = requests.get("https://corona.lmao.ninja/v2/all").json()
+        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    else:
+        variabla = text[1]
+        r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
+        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid, run_async = True)
+dispatcher.add_handler(COVID_HANDLER)
+
+__mod_name__ = "COVID 19"
+__help__ = """
+*COVID 19*
+/corona - Gives information regarding covid 19 cases
+/covid - Gives information regarding covid 19 cases
+"""
---
+++ Shikimori/modules/games.py
@@ -0,0 +1,41 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Shikimori import DEV_USERS, SUPPORT_USERS,pbot as app
+
+SUDOERS = SUPPORT_USERS.append(DEV_USERS)
+
+__mod_name__ = "GAMEY 🎁"
+__help__ = """
+Use these commands and try to score.\n\n
+/dice - Dice 🎲\n
+/dart - Dart 🎯\n
+/basket - Basket Ball 🏀\n
+/bowling - Bowling Ball 🎳\n
+/football - Football ⚽\n
+/slot - Spin slot machine 🎰
+"""
+
+@app.on_message(filters.command("dice"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎲")
+
+@app.on_message(filters.command("dart"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎯")
+
+@app.on_message(filters.command("basket"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🏀")
+
+@app.on_message(filters.command("bowling"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎳")
+
+@app.on_message(filters.command("football"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "⚽")
+
+@app.on_message(filters.command("slot"))
+async def throw_dice(client, message: Message): 
+    await client.send_dice(message.chat.id, "🎰")
