03f27123801bdab9ad87ff2e9bff096eecb61605
Yash <yashprakash2005@gmail.com>
2022-05-25 07:25:21 +0530

    Update userinfo.py

--- Shikimori/modules/userinfo.py
+++ Shikimori/modules/userinfo.py
@@ -116,8 +116,8 @@ def hpmanager(user):
         if not sql.get_user_bio(user.id):
             new_hp -= no_by_per(total_hp, 10)
 
-        if is_afk(user.id):
-            afkst = check_afk_status(user.id)
+        if is_user_afk(user.id):
+            afkst = afk_reason(user.id)
             # if user is afk and no reason then decrease 7%
             # else if reason exist decrease 5%
             new_hp -= no_by_per(total_hp, 7) if not afkst else no_by_per(total_hp, 5)
