a1e0e72f0ab0dd366c1580f996aadff0a29e0dbc
Yash <yashprakash2005@gmail.com>
2022-05-25 17:01:04 +0530

    Update userinfo.py

--- Shikimori/modules/userinfo.py
+++ Shikimori/modules/userinfo.py
@@ -287,7 +287,7 @@ def info(update: Update, context: CallbackContext):
     if chat.type != "private" and user_id != bot.id:
         _stext = "\n• Presence: <code>{}</code>"
 
-        afk_st = is_afk(user.id)
+        afk_st = is_user_afk(user.id)
         if afk_st:
             text += _stext.format("AFK")
         else:
