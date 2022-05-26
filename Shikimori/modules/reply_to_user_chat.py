a1e4f7b5ed79d56698793d676a9f820e5da88108
Yash <yashprakash2005@gmail.com>
2022-05-27 04:07:42 +0530

    Update reply_to_user_chat.py

--- Shikimori/modules/reply_to_user_chat.py
+++ Shikimori/modules/reply_to_user_chat.py
@@ -7,7 +7,8 @@ from telegram.ext import CallbackContext, Filters, run_async
 
 IMG_GM = "https://telegra.ph/file/fff37608fa21d9d3d0b39.jpg"
 IMG_GN = "https://telegra.ph/file/1862c7260109e24ed4715.jpg"
-
+IMG_HELLO = "https://telegra.ph/file/f3f2dc386a33e37f6cb05.png"
+IMG_BYE = "https://telegra.ph/file/061054c8f73fe7ffbf6aa.mp4"
 
 def goodnight(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -34,6 +35,30 @@ def goodmorning(update, context):
         reply = f"*Good Morning:* {user1}"
         message.reply_text(reply)
 
+def hello(update: Update, context: CallbackContext):
+    message = update.effective_message
+    user1 = message.from_user.first_name
+    try:
+        update.effective_message.reply_photo(
+            IMG_HELLO,f"*Hello* {user1}",
+            parse_mode=ParseMode.MARKDOWN,
+        )
+    except:
+        reply = f"*Hello* {user1}"
+        message.reply_text(reply)
+
+def bye(update: Update, context: CallbackContext):
+    message = update.effective_message
+    user1 = message.from_user.first_name
+    try:
+        update.effective_message.reply_photo(
+            IMG_BYE,f"*Bye!!* {user1}",
+            parse_mode=ParseMode.MARKDOWN,
+        )
+    except:
+        reply = f"*Bye!!* {user1}"
+        message.reply_text(reply)
+
 
 
 GDMORNING_HANDLER = DisableAbleMessageHandler(
@@ -42,6 +67,14 @@ GDMORNING_HANDLER = DisableAbleMessageHandler(
 GDNIGHT_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"(?i)(good night|gn|goodnight)"), goodnight, friendly="goodnight", run_async = True
 )
+BYE_HANDLER = DisableAbleMessageHandler(
+    Filters.regex(r"(?i)(bye)"), goodnight, friendly="bye", run_async = True
+)
+HELLO_HANDLER = DisableAbleMessageHandler(
+    Filters.regex(r"(?i)(hello)"), goodnight, friendly="hello", run_async = True
+)
 
 dispatcher.add_handler(GDMORNING_HANDLER)
 dispatcher.add_handler(GDNIGHT_HANDLER)
+dispatcher.add_handler(HELLO_HANDLER)
+dispatcher.add_handler(BYE_HANDLER)
