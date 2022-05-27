673a7273f3427bdf64a8cadffae587b1b7beb94d
Yash <yashprakash2005@gmail.com>
2022-05-27 05:55:39 +0530

    Update chatbot.py

--- Shikimori/modules/chatbot.py
+++ Shikimori/modules/chatbot.py
@@ -26,39 +26,47 @@ from asyncio import gather, sleep
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Shikimori import (pbot as app, BOT_ID)
-from Shikimori.utils.errors import capture_err
-from Shikimori.utils.filter_groups import chatbot_group
-from Shikimori import arq
+from wbb import (
+    BOT_ID,
+    SUDOERS,
+    USERBOT_ID,
+    USERBOT_PREFIX,
+    USERBOT_USERNAME,
+    app,
+    app2,
+    arq,
+    eor,
+)
+from wbb.core.decorators.errors import capture_err
+from wbb.utils.filter_groups import chatbot_group
 
+__MODULE__ = "ChatBot"
+__HELP__ = """
+/chatbot [ENABLE|DISABLE] To Enable Or Disable ChatBot In Your Chat.
 
-from inspect import getfullargspec
-
-async def eor(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    return await func(**{k: v for k, v in kwargs.items() if k in spec})
-
+There's one module of this available for userbot also
+check userbot module help."""
 
 active_chats_bot = []
+active_chats_ubot = []
 
 
 async def chat_bot_toggle(db, message: Message):
     status = message.text.split(None, 1)[1].lower()
     chat_id = message.chat.id
-    if status == "on":
+    if status == "enable":
         if chat_id not in db:
             db.append(chat_id)
             text = "Chatbot Enabled!"
             return await eor(message, text=text)
         await eor(message, text="ChatBot Is Already Enabled.")
-    elif status == "off":
+    elif status == "disable":
         if chat_id in db:
             db.remove(chat_id)
             return await eor(message, text="Chatbot Disabled!")
         await eor(message, text="ChatBot Is Already Disabled.")
     else:
-        await eor(message, text="**Usage:**\n/chatbot [ON|OFF]")
+        await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
 
 
 # Enabled | Disable Chatbot
@@ -92,7 +100,8 @@ async def type_and_send(message: Message):
     & filters.reply
     & ~filters.bot
     & ~filters.via_bot
-    & ~filters.forwarded,
+    & ~filters.forwarded
+    & ~filters.edited,
     group=chatbot_group,
 )
 @capture_err
@@ -108,3 +117,50 @@ async def chatbot_talk(_, message: Message):
     await type_and_send(message)
 
 
+# FOR USERBOT
+
+
+@app2.on_message(
+    filters.command("chatbot", prefixes=USERBOT_PREFIX)
+    & ~filters.edited
+    & SUDOERS
+)
+@capture_err
+async def chatbot_status_ubot(_, message: Message):
+    if len(message.text.split()) != 2:
+        return await eor(message, text="**Usage:**\n.chatbot [ENABLE|DISABLE]")
+    await chat_bot_toggle(active_chats_ubot, message)
+
+
+@app2.on_message(
+    ~filters.me & ~filters.private & filters.text & ~filters.edited,
+    group=chatbot_group,
+)
+@capture_err
+async def chatbot_talk_ubot(_, message: Message):
+    if message.chat.id not in active_chats_ubot:
+        return
+    username = "@" + str(USERBOT_USERNAME)
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user:
+            return
+        if (
+                message.reply_to_message.from_user.id != USERBOT_ID
+                and username not in message.text
+        ):
+            return
+    else:
+        if username not in message.text:
+            return
+    await type_and_send(message)
+
+
+@app2.on_message(
+    filters.text & filters.private & ~filters.me & ~filters.edited,
+    group=(chatbot_group + 1),
+)
+@capture_err
+async def chatbot_talk_ubot_pm(_, message: Message):
+    if message.chat.id not in active_chats_ubot:
+        return
+    await type_and_send(message)
