4385aeadb4bbae0eb1539544bbb287ebb3d6b4dd
Yash <yashprakash2005@gmail.com>
2022-05-29 03:20:55 +0530

    Update animelinks.py

--- Shikimori/modules/animelinks.py
+++ Shikimori/modules/animelinks.py
@@ -2,6 +2,8 @@ from telegram import InlineKeyboardButton
 from Shikimori.modules.animedev import client as animedev_client, exceptions
 from Shikimori.events import register
 from Shikimori import telethn
+from telethon import Button
+
 
 @register(pattern='/anilink')
 async def animelink(event):
@@ -19,20 +21,10 @@ async def animelink(event):
         await event.reply(f'*Error*: Contact @tyranteyeeee.\nERROR: {e}')
         return
     text = f'''
-                *Anime Title*: {anime['AnimeTitle']}
-
-                *Search Query*: {anime['Search_Query']}
-                '''
-    buttons = [
-    [
-        InlineKeyboardButton(
-            text="Link", url= anime['AnimeLink']),
-    ],
-    [
-        InlineKeyboardButton(text="Search Query", url= anime['Search_Query']),
-    ],
-    ]
+<b>Anime Title:</b> <code>{anime['AnimeTitle']}</code>
+    '''
+    button_list = [[Button.url('Download Link', anime['AnimeLink'])], [Button.url('Search Query', anime['Search_Query'])]]
     
-    await telethn.send_file(event.chat_id, anime['AnimeImg'], caption=text, buttons = buttons)
+    await telethn.send_file(event.chat_id, anime['AnimeImg'], caption=text, buttons=button_list, parse_mode='html', reply_to=event.id)
 
 
