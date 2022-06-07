6aedd46087dfceff77d3c9d395ee21b3b70f5520
Yash <yashprakash2005@gmail.com>
2022-06-08 00:31:20 +0530

    hmm

--- Shikimori/modules/logomaker.py
+++ Shikimori/modules/logomaker.py
@@ -155,7 +155,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Shikimori/resources/AceRecords.ttf", 330)
+    font = ImageFont.truetype("./Shikimori/resources/Bombing.ttf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
