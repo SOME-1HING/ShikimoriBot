130528d6d7ed61644b2efa6388f73a69a5181fa6
Yash <yashprakash2005@gmail.com>
2022-06-09 04:43:30 +0530

    Create pfp.py

---
+++ Shikimori/modules/pfp.py
@@ -0,0 +1,48 @@
+from telethon import *
+from telethon.tl.functions.account import *
+from telethon.tl.functions.channels import *
+from telethon.tl.functions.photos import *
+from telethon.tl.types import *
+from Shikimori.events import register
+from Shikimori import telethn as borg
+from html import *
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+if 1 == 1:
+    name = "Profile Photos"
+    client = borg
+
+    @register(pattern=("/pfp"))
+    async def PPScmd(event):
+#        """Gets the profile photos of replied users, channels or chats"""
+        id = "".join(event.raw_text.split(maxsplit=2)[1:]) 
+        user = await event.get_reply_message()
+        if user:
+            photos = await event.client.get_profile_photos(user.sender)
+        else:
+            photos = await event.client.get_profile_photos(event.chat_id)
+        if id.strip() == "":
+            try:
+                await event.client.send_file(event.chat.id, photos)
+            except a:
+                photo = await event.client.download_profile_photo(event.chat_id)
+                await borg.send_file(event.chat.id, photo)
+        else:
+            try:
+                id = int(id)
+                if id <= 0:
+                    await event.edit("<code>ID number you entered is invalid</code>")
+                    return
+            except:
+                 await event.edit("<code>ID number you entered is invalid</code>")
+                 return
+            if int(id) <= (len(photos)):
+                send_photos = await event.client.download_media(photos[id - 1])
+                await borg.send_file(event.chat.id, send_photos)
+            else:
+                await event.edit("<code>No photo found with that id</code>")
+                return
