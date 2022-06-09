c4e49ccf2fd3d45a9de679711935de5282a28191
Yash <yashprakash2005@gmail.com>
2022-06-09 15:24:45 +0530

    Update antiservice.py

--- Shikimori/modules/antiservice.py
+++ Shikimori/modules/antiservice.py
@@ -15,7 +15,7 @@ Plugin to delete service messages in a chat!
 """
 
 
-@app.on_message(filters.command("antiservice") & ~filters.private)
+@app.on_message(filters.command("antiservice") & filters.private)
 @adminsOnly("can_change_info")
 async def anti_service(_, message):
     await message.reply_text('pro coder')
