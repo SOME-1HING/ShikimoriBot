fcda9c9b1d70729da4d729604d94c0e609b4b76e
testingXbots <107467786+testingXbots@users.noreply.github.com>
2022-06-15 02:13:04 +0530

    Update __main__.py

--- Shikimori/__main__.py
+++ Shikimori/__main__.py
@@ -486,8 +486,8 @@ def get_help(update: Update, context: CallbackContext):
                 ),
             )
             return
-       update.effective_message.reply_photo(
-            HELP_IMG, caption= "Contact me in PM to get the list of possible commands.",
+       update.effective_message.reply_text(
+            "Contact me in PM to get the list of possible commands.",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
