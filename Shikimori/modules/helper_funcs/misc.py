ff551b0bb77173cb71ab69499f907c37413378e0
testingXbots <107467786+testingXbots@users.noreply.github.com>
2022-06-15 10:10:00 +0530

    Update misc.py

--- Shikimori/modules/helper_funcs/misc.py
+++ Shikimori/modules/helper_funcs/misc.py
@@ -1,9 +1,19 @@
-from typing import Dict, List
 from math import ceil
+from typing import Dict, List
+from uuid import uuid4
+
+from telegram import (
+    MAX_MESSAGE_LENGTH,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    ParseMode,
+)
+from telegram.error import TelegramError
 
 from Shikimori import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
-from telegram.error import TelegramError
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -37,48 +47,7 @@ def split_message(msg: str) -> List[str]:
     return result
 
 
-# def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
-#     if not chat:
-#         modules = sorted(
-#             [
-#                 EqInlineKeyboardButton(
-#                     x.__mod_name__,
-#                     callback_data="{}_module({})".format(
-#                         prefix, x.__mod_name__.lower()
-#                     ),
-#                 )
-#                 for x in module_dict.values()
-#             ]
-#         )
-#     else:
-#         modules = sorted(
-#             [
-#                 EqInlineKeyboardButton(
-#                     x.__mod_name__,
-#                     callback_data="{}_module({},{})".format(
-#                         prefix, chat, x.__mod_name__.lower()
-#                     ),
-#                 )
-#                 for x in module_dict.values()
-#             ]
-#         )
-
-#     pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
-
-#     round_num = len(modules) / 3
-#     calc = len(modules) - round(round_num)
-#     if calc in [1, 2]:
-#         pairs.append((modules[-1],))
-#     elif calc == 2:
-#         pairs.append((modules[-1],))
-
-#     else:
-#         pairs += [[EqInlineKeyboardButton("Go Home", callback_data="Shikimori_back")]]
-
-#     return pairs
-
-
-def paginate_modules(page_n, module_dict, prefix, chat=None):
+def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
             [
@@ -104,42 +73,58 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
-    i = 0
-    for m in pairs:
-        for _ in m:
-            i += 1
-    if len(modules) - i == 1:
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc in [1, 2]:
         pairs.append((modules[-1],))
-    elif len(modules) - i == 2:
-        pairs.append(
-            (
-                modules[-2],
-                modules[-1],
-            )
-        )
 
-    COLUMN_SIZE = 8
-
-    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    max_num_pages = ceil(len(pairs) / 6)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[
-            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
-        ] + [
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
             (
-                EqInlineKeyboardButton("❮", callback_data="{}_prev({})".format(prefix, modulo_page),),
-                EqInlineKeyboardButton("Go Home", callback_data="Shikimori_back"),
-                EqInlineKeyboardButton("❯", callback_data="{}_next({})".format(prefix, modulo_page),),
+                EqInlineKeyboardButton(
+                    "⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("Close", callback_data="Shikimori_back"),
+                EqInlineKeyboardButton(
+                    "➡️", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
             )
         ]
+
+    else:
+        pairs += [[EqInlineKeyboardButton("Close", callback_data="Shikimori_back")]]
 
     return pairs
 
 
-    
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
+
+
 def send_to_list(
     bot: Bot, send_to: list, message: str, markdown=False, html=False
 ) -> None:
@@ -188,6 +173,29 @@ def build_keyboard_parser(bot, chat_id, buttons):
             keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
         else:
             keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
+def user_bot_owner(func):
+    @wraps(func)
+    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == OWNER_ID:
+            return func(bot, update, *args, **kwargs)
+        else:
+            pass
+
+    return is_user_bot_owner
+
+
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
 
     return keyb
 
