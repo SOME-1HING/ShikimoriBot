e7be3071c5b8e1ff30f14cdb76098b360f281c5e
AUGSTUN <97671641+AUGSTUN@users.noreply.github.com>
2022-07-07 16:19:39 +0530

    Update reply_to_user_chat.py

--- Shikimori/modules/reply_to_user_chat.py
+++ Shikimori/modules/reply_to_user_chat.py
@@ -12,7 +12,7 @@ from telegram.ext import CallbackContext, Filters
 IMG_GM = "https://telegra.ph/file/fff37608fa21d9d3d0b39.jpg"
 IMG_GN = "https://telegra.ph/file/1862c7260109e24ed4715.jpg"
 IMG_HELLO = "https://telegra.ph/file/f3f2dc386a33e37f6cb05.png"
-IMG_BYE = "https://telegra.ph/file/061054c8f73fe7ffbf6aa.mp4"
+IMG_BYE = "https://te.legra.ph/file/9d570141d0b411f2b77bc.mp4"
 
 def goodnight(update: Update, context: CallbackContext):
     message = update.effective_message
