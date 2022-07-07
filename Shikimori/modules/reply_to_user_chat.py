0bc6549e6a3a6c022515552fa754d29eac9c323d
AUGSTUN <97671641+AUGSTUN@users.noreply.github.com>
2022-07-07 22:26:16 +0530

    Update reply_to_user_chat.py

--- Shikimori/modules/reply_to_user_chat.py
+++ Shikimori/modules/reply_to_user_chat.py
@@ -73,14 +73,19 @@ def amazing(update: Update, context: CallbackContext):
             IMG_AMAZING,
             parse_mode=ParseMode.MARKDOWN,
         ) 
-        
-
 def study(update: Update, context: CallbackContext):
     message = update.effective_message
+    user1 = message.from_user.first_name
     try:
-        update.effective_message.reply_animatio(
+        update.effective_message.reply_animation(
+            IMG_STUDY, caption = f"*JaPadle!!* {user1}",
             parse_mode=ParseMode.MARKDOWN,
-        ) 
+        )
+    except:
+        reply = f"*JaPadle!!* {user1}"
+        message.reply_text(reply)       
+
+
 
 
 GDMORNING_HANDLER = DisableAbleMessageHandler(
