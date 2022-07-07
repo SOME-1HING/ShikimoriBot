8908ba2b67145e6c45ec981cee026677cbc191f5
Himanshu Mishra <108786118+HimasnhuMishra9711@users.noreply.github.com>
2022-07-07 22:08:52 +0530

    Update reply_to_user_chat.py

--- Shikimori/modules/reply_to_user_chat.py
+++ Shikimori/modules/reply_to_user_chat.py
@@ -77,15 +77,10 @@ def amazing(update: Update, context: CallbackContext):
 
 def study(update: Update, context: CallbackContext):
     message = update.effective_message
-    user1 = message.from_user.first_name
     try:
-        update.effective_message.reply_animation(
-            IMG_STUDY, caption = f"*JaPadle!!* {user1}",
+        update.effective_message.reply_animatio(
             parse_mode=ParseMode.MARKDOWN,
-        )
-    except:
-        reply = f"*JaPadle!!* {user1}"
-        message.reply_text(reply)
+        ) 
 
 
 GDMORNING_HANDLER = DisableAbleMessageHandler(
