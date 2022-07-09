e0cea752e611bb22dd7d6ce62511dc263213bd07
SOME1HING <yashprakash2005@gmail.com>
2022-07-09 21:30:13 +0530

    Update apod.py

--- Shikimori/modules/apod.py
+++ Shikimori/modules/apod.py
@@ -1,5 +1,5 @@
 from Shikimori import dispatcher, APOD_API_KEY
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
 from telegram.ext import (
     CallbackContext,
     CommandHandler,
@@ -12,13 +12,14 @@ def apod(update: Update, context: CallbackContext):
     title = result['title']
     copyright = result['copyright']
     url = 'https://apod.nasa.gov/apod/'
-    text = f'Title: {title}\n\nCredits: {copyright}'
+    text = f'<b>Title: <u>{title}</u></b>\n\n<i>Credits: {copyright}</i>'
     
     update.effective_message.reply_photo(img, caption=text, reply_markup=InlineKeyboardMarkup(
         [    
             [InlineKeyboardButton("More Info" , url=url)]
 
-        ]))
+        ]),
+        parse_mode=ParseMode.HTML)
 
 apod_handler = CommandHandler("apod", apod, run_async = True)
 dispatcher.add_handler(apod_handler)
