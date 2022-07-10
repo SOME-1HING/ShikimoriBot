0d08cea08a3458564e51c48bc0fbe7a1fafe5d45
SOME1HING <yashprakash2005@gmail.com>
2022-07-11 05:29:58 +0530

    Update bug.py

--- Shikimori/modules/bug.py
+++ Shikimori/modules/bug.py
@@ -1,20 +1,14 @@
 from datetime import datetime
-
-from pyrogram import filters
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
-
-from Shikimori import DEV_USERS, OWNER_ID, STATS_IMG, pbot
+from Shikimori import DEV_USERS, OWNER_ID, STATS_IMG
 from Shikimori import (
     OWNER_ID,
     OWNER_USERNAME,
     SUPPORT_CHAT,
+    dispatcher
 )
-from Shikimori.utils.errors import capture_err
+import time
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CallbackQueryHandler
 
 
 def content(msg: Message) -> [None, str]:
@@ -30,18 +24,15 @@ def content(msg: Message) -> [None, str]:
     else:
         return None
 
-
-@pbot.on_message(filters.command("bug"))
-@capture_err
-async def bug(_, msg: Message):
-    if msg.chat.username:
-        chat_username = (f"@{msg.chat.username} / `{msg.chat.id}`")
-    else:
-        chat_username = (f"Private Group / `{msg.chat.id}`")
-
-    bugs = content(msg)
-    user_id = msg.from_user.id
-    mention = "["+msg.from_user.first_name+"](tg://user?id="+str(msg.from_user.id)+")"
+def bug(update: Update, context: CallbackContext):
+    args = context.args
+    user_id = update.effective_message.from_user.id
+    message = update.effective_message
+    chat_id = update.effective_chat.id
+    if message.chat.username:
+        chat_username = (f"@{dispatcher.bot.getChat(chat_id).username} / `{dispatcher.bot.getChat(chat_id).id}`")
+    bugs = content(message)
+    mention = "["+update.effective_user.first_name+"](tg://user?id="+str(message.from_user.id)+")"
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
     bug_report = f"""
@@ -52,24 +43,22 @@ async def bug(_, msg: Message):
 **Bug Report : ** **{bugs}**
 **Event Stamp : ** **{datetimes}**"""
 
-    
-    if msg.chat.type == "private":
-        await msg.reply_text(f"❎ <b>This command only works in groups.</b>\n\n Visit @{SUPPORT_CHAT} to report bugs related to bot's pm.")
+    if update.effective_chat.type == "private":
+        update.effective_message.reply_text(f"❎ <b>This command only works in groups.</b>\n\n Visit @{SUPPORT_CHAT} to report bugs related to bot's pm.")
         return
-
     if user_id == OWNER_ID:
         if bugs:
-            await msg.reply_text(
+            message.reply_text(
                 "❎ <b>Why owner of bot reporting a bug?? Go fix yourself</b>",
             )
             return
         else:
-            await msg.reply_text(
+            message.reply_text(
                 "❎ <b>Why owner of bot reporting a bug?? Go fix yourself</b>"
             )
     elif user_id != OWNER_ID:
         if bugs:
-            await msg.reply_text(
+            message.reply_text(
                 f"<b>Bug Report : {bugs}</b>\n\n"
                 "✅ <b>The bug was successfully reported to the support group!</b>",
                 reply_markup=InlineKeyboardMarkup(
@@ -81,7 +70,8 @@ async def bug(_, msg: Message):
                     ]
                 )
             )
-            await pbot.send_photo(
+
+            message.reply_photo(
                 SUPPORT_CHAT,
                 photo=STATS_IMG,
                 caption=f"{bug_report}",
@@ -89,7 +79,7 @@ async def bug(_, msg: Message):
                     [
                         [
                             InlineKeyboardButton(
-                                "➡ View Bug", url=f"{msg.link}")
+                                "➡ View Bug", url=f"{message.link}")
                         ],
                         [
                             InlineKeyboardButton(
@@ -99,39 +89,37 @@ async def bug(_, msg: Message):
                 )
             )
         else:
-            await msg.reply_text(
+            message.reply_text(
                 f"❎ <b>No bug to Report!</b> Use `/bug <information>`",
             )
-        
-# @pbot.on_callback_query(filters.regex("close_reply"))
-# async def close_reply(msg, CallbackQuery):
-#     await CallbackQuery.message.delete()
 
-# @pbot.on_callback_query(filters.regex("close_send_photo"))
-# async def close_send_photo(_, CallbackQuery):
-#     is_Admin = await pbot.get_chat_member(
-#         CallbackQuery.message.chat.id, CallbackQuery.from_user.id
-#     )
-#     if not is_Admin.can_delete_messages:
-#         return await CallbackQuery.answer(
-#             "You're not allowed to close this.", show_alert=True
-#         )
-#     else:
-#         await CallbackQuery.message.delete()
+def close_reply(update: Update, context: CallbackContext):
+    query = update.callback_query
+    if query.data == "close_reply_":
+        query.message.delete()
 
-@pbot.on_callback_query(filters.regex("close_reply"))
-async def close_reply(client: pbot, query: CallbackQuery):
-    await query.message.delete()
-
-@pbot.on_callback_query(filters.regex("close_send_photo"))
-async def close_send_photo(client: pbot, query: CallbackQuery):
-    user_id = query.from_user.id
-    if user_id not in DEV_USERS :
-        return await query.message(
-            "Only developers can close Bug reports.", show_alert=True
+def close_send_photo(update: Update, context: CallbackContext):
+    query = update.callback_query
+    if query.data == "close_send_photo_":
+        user = update.effective_user
+        msg = message = update.effective_message
+        if user.id not in DEV_USERS :
+            msg.reply_text(
+            "Only developers can close Bug reports."
         )
-    else:
-        await query.message.delete()
+            time.sleep(10)
+            msg.delete()
+            return
+
+        else:
+            query.message.delete()
+
+close_reply_handler = CallbackQueryHandler(
+        close_reply, pattern=r"close_reply_", run_async=True
+    )
+close_send_photo_handler = CallbackQueryHandler(
+    close_send_photo, pattern=r"close_send_photo_", run_async=True
+)
 
 __mod_name__ = "Bug"
 
