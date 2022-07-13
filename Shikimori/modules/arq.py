98c03845326aa9949e568cd03797bdde197fffb7
SOME1HING <yashprakash2005@gmail.com>
2022-07-14 05:12:21 +0530

    Update arq.py

--- Shikimori/modules/arq.py
+++ Shikimori/modules/arq.py
@@ -1,7 +1,6 @@
 from pyrogram import filters
-from Shikimori.utils.pastebin import paste
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Shikimori import pbot
+from Shikimori import SUPPORT_CHAT, pbot
 from Shikimori.utils.arqapi import arq
 
 @pbot.on_message(filters.command("torrent"))
@@ -10,22 +9,27 @@ async def torrent_func(_, message):
         return await message.reply_text("**Usage:**\n/torrent [QUERY]")
     m = await message.reply_text("**Searching**")
     query = message.text.strip().split(None, 1)[1]
-    hmm = await arq.torrent(query)
-    torrent = hmm.result[0]
-    name = torrent["name"]
-    upload = torrent["uploaded"]
-    size = torrent["size"]
-    seeds = torrent["seeds"]
-    leechs = torrent["leechs"]
-    magnet = torrent["magnet"]
 
-    text = f"Title - **{name}**\n"
-    text += f"Uploaded By - `{upload}`\n"
-    text += f"Size - `{size}`\n"
-    text += f"Seeds - `{seeds}`  Leechs - `{leechs}`\n\n"
-    text += f"Torrent Magnet - `{magnet}`\n"
+    try:
+        hmm = await arq.torrent(query)
+        torrent = hmm.result[0]
+        name = torrent["name"]
+        upload = torrent["uploaded"]
+        size = torrent["size"]
+        seeds = torrent["seeds"]
+        leechs = torrent["leechs"]
+        magnet = torrent["magnet"]
 
-    await m.edit(text)   
+        text = f"Title - **{name}**\n"
+        text += f"Uploaded On - `{upload}`\n"
+        text += f"Size - `{size}`\n"
+        text += f"Seeds - `{seeds}`  Leechs - `{leechs}`\n\n"
+        text += f"Torrent Magnet - `{magnet}`\n"
+
+        await m.edit(text)   
+    
+    except:
+        await m.edit(f"Check your query. And if the function doesn't work, contact @{SUPPORT_CHAT}")
 
 @pbot.on_message(filters.command("yt"))
 async def ytarq(_, message):
@@ -34,29 +38,54 @@ async def ytarq(_, message):
     m = await message.reply_text("**Searching**")
     query = message.text.strip().split(None, 1)[1]
 
-    hmm = await arq.youtube(query)
-    videos = hmm.result[0]
-    thumbs = videos["thumbnails"]
-    thumb= thumbs[0]
-    thumb = thumb + ".jpg"
-    title = videos["title"]
-    channel = videos["channel"]
-    Duration = videos["duration"]
-    views = videos["views"]
-    url = videos["url_suffix"]
-    views = views.replace('.', ",")
-    views = views.replace("Aufrufe", "")
+    try:
+        hmm = await arq.youtube(query)
+        videos = hmm.result[0]
+        thumbs = videos["thumbnails"]
+        thumb= thumbs[0]
+        thumb = thumb + ".jpg"
+        title = videos["title"]
+        channel = videos["channel"]
+        Duration = videos["duration"]
+        views = videos["views"]
+        url = videos["url_suffix"]
+        views = views.replace('.', ",")
+        views = views.replace("Aufrufe", "")
 
-    text = f"Title 🎩 - **{title}**\n"
-    text += f"Duration 🕔 - `{Duration}`\n"
-    text += f"Views 👀 - `{views}`\n"
-    text += f"Channel 📺 - `{channel}`\n"
-    link = f"https://youtube.com{url}"
+        text = f"Title 🎩 - **{title}**\n"
+        text += f"Duration 🕔 - `{Duration}`\n"
+        text += f"Views 👀 - `{views}`\n"
+        text += f"Channel 📺 - `{channel}`\n"
+        link = f"https://youtube.com{url}"
 
-    buttons = [
-        [
-            InlineKeyboardButton(text="Visit", url=link),
-        ],]
-    await m.delete()
-    return await message.reply_photo(thumb ,caption = text, reply_markup=InlineKeyboardMarkup(buttons))
-  
+        buttons = [
+            [
+                InlineKeyboardButton(text="Visit", url=link),
+            ],]
+        await m.delete()
+        return await message.reply_photo(thumb ,caption = text, reply_markup=InlineKeyboardMarkup(buttons))
+    
+    except:
+        await m.edit(f"ERROR!!! Contact @{SUPPORT_CHAT}")
+
+
+@pbot.on_message(filters.command("spellcheck"))
+async def torrent_func(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Usage:**\n/spellcheck [QUERY]")
+
+    query = message.text.strip().split(None, 1)[1]
+
+    try:
+        hmm = await arq.spellcheck(query)
+        spellcheck = hmm.result[0]
+        corrected = spellcheck["corrected"]
+        corrections = spellcheck["corrections"]
+
+        text = f"corrected - **{corrected}**\n"
+        text += f"corrections - `{corrections}`\n"
+
+        await message.reply_text(text)   
+    
+    except:
+        await message.reply_text(f"Check your query. And if the function doesn't work, contact @{SUPPORT_CHAT}")
