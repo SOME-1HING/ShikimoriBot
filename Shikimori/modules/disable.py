e79031a0d9587885da1d58a36e635e00689bdfc3
SOME1HING <yashprakash2005@gmail.com>
2022-07-15 05:39:39 +0530

    Fixed minor errors

--- Shikimori/modules/disable.py
+++ Shikimori/modules/disable.py
@@ -191,7 +191,7 @@ if is_module_loaded(FILENAME):
         args = context.args
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "SaitamaRobot.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "Shikimori.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
@@ -265,7 +265,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "SaitamaRobot.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "Shikimori.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
