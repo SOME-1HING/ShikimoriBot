424a109159a7aac2ddb5002c97c68b0a41060ffb
SOME1HING <yashprakash2005@gmail.com>
2022-07-15 21:07:38 +0530

    Update antinsfw.py

--- Shikimori/modules/antinsfw.py
+++ Shikimori/modules/antinsfw.py
@@ -39,13 +39,14 @@ MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 bot_name = f"{dispatcher.bot.first_name}"
 
 antinsfw_chats = db.antinsfw
+antislang_chats = db.antislang
 
 CMD_STARTERS = "/"
 better_profanity.profanity.load_censor_words_from_file("./Shikimori/Extras/profanity_wordlist.txt")
 
 
 @register(pattern="^/antinsfw(?: |$)(.*)")
-async def profanity(event):
+async def antinsfw(event):
     if event.fwd_from:
         return
     if not event.is_group:
@@ -105,19 +106,6 @@ async def del_nsfw(event):
         return
     chats = antinsfw_chats.find({})
     for c in chats:
-        if event.text:
-            if event.chat_id == c["id"]:
-                if better_profanity.profanity.contains_profanity(msg):
-                    await event.delete()
-                    if sender.username is None:
-                        st = sender.first_name
-                        hh = sender.id
-                        final = f"[{st}](tg://user?id={hh}) **{msg}** is detected as a slang word and your message has been deleted"
-                    else:
-                        final = f"Sir **{msg}** is detected as a slang word and your message has been deleted"
-                    dev = await event.respond(final)
-                    await asyncio.sleep(10)
-                    await dev.delete()
         if not event.text:
             is_nsfw = sql.is_nsfw(c["id"])
             if not is_nsfw:
@@ -143,12 +131,10 @@ async def del_nsfw(event):
                     await event.respond("hmm") 
                     if results.ok:
                         return
-                    await event.respond(results.porn)
-                    await event.respond(float(results.porn)) 
-                    hmm = float(results.hentai)
-                    hmmm = float(results.porn)
-                    hmmmm = float(results.sexy)
-                    if hmm >= 52.000 or hmmm >= 15.000 or hmmmm >= 30.000:
+                    await event.respond(f"{results.porn}")
+                    check = f"{results.is_nsfw}"
+
+                    if "True" in check:
                         await event.delete()
                         st = sender.first_name
                         hh = sender.id
@@ -161,4 +147,80 @@ async def del_nsfw(event):
                 except Exception:
                     return  
 
+
+
+@register(pattern="^/antislang(?: |$)(.*)")
+async def antislang(event):
+    if event.fwd_from:
+        return
+    if not event.is_group:
+        await event.reply("You Can Only antislang in Groups.")
+        return
+    event.pattern_match.group(1)
+    if await is_admin(event, event.message.sender_id):
+        input = event.pattern_match.group(1)
+        chats = antislang_chats.find({})
+        if not input:
+            for c in chats:
+                if event.chat_id == c["id"]:
+                    await event.reply(
+                        "Please provide some input yes or no.\n\nCurrent setting is : **on**"
+                    )
+                    return
+            await event.reply(
+                "Please provide some input yes or no.\n\nCurrent setting is : **off**"
+            )
+            return
+        if input == "on":
+            if event.is_group:
+                chats = antislang_chats.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        await event.reply(
+                            "Profanity filter is already activated for this chat."
+                        )
+                        return
+                antislang_chats.insert_one({"id": event.chat_id})
+                await event.reply("Profanity filter turned on for this chat.")
+        elif input == "off":
+            if event.is_group:
+                chats = antislang_chats.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        antislang_chats.delete_one({"id": event.chat_id})
+                        await event.reply("Profanity filter turned off for this chat.")
+                        return
+            await event.reply("Profanity filter isn't turned on for this chat.")
+        else:
+            await event.reply("I only understand by on or off")
+            return
+    else:
+        await event.reply("`You Should Be Admin To Do This!`")
+        return
  
+@loggable
+@tbot.on(events.NewMessage(pattern=None))
+async def del_profanity(event):
+    if event.is_private:
+        return
+    msg = str(event.text)
+    sender = await event.get_sender()
+    # let = sender.username
+    if await is_admin(event, event.message.sender_id):
+        return
+    chats = antislang_chats.find({})
+    for c in chats:
+        if event.text:
+            if event.chat_id == c["id"]:
+                if better_profanity.profanity.contains_profanity(msg):
+                    await event.delete()
+                    if sender.username is None:
+                        st = sender.first_name
+                        hh = sender.id
+                        final = f"[{st}](tg://user?id={hh}) used word: **{msg}** which is detected as a slang word and his message has been deleted."
+                    else:
+                        final = f"[{st}](tg://user?id={hh}) used word: **{msg}** which is detected as a slang word and his message has been deleted."
+                    dev = await event.respond(final)
+                    await asyncio.sleep(10)
+                    await dev.delete()
+                    return dev
