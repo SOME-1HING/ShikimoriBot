1213659176b59db0b62e94c24736afa082782c6a
SOME1HING <yashprakash2005@gmail.com>
2022-07-16 12:43:55 +0530

    Update aservice.py

--- Shikimori/modules/aservice.py
+++ Shikimori/modules/aservice.py
@@ -18,27 +18,30 @@ Plugin to delete service messages in a chat!
 @user_admin
 @pbot.on_message(filters.command("antiservice") & filters.group)
 async def aservice_state(_, message):
-    usage = "**Usage:**\n/antiservice [ON|OFF]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    chat_id = message.chat.id
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
-    if state == "on":
-        is_aservice = sql.is_aservice(chat_id)
-        if not is_aservice:
-            sql.set_aservice(chat_id)
-        await message.reply_text("Enabled AntiService System. I will Delete Service Messages from Now on.")
-    elif state == "off":
-        is_aservice = sql.is_aservice(chat_id)
-        if not is_aservice:
-            await message.reply_text("AntiService System is already disabled.")
-            return ""
+    try:
+        usage = "**Usage:**\n/antiservice [ON|OFF]"
+        if len(message.command) != 2:
+            return await message.reply_text(usage)
+        chat_id = message.chat.id
+        state = message.text.split(None, 1)[1].strip()
+        state = state.lower()
+        if state == "on":
+            is_aservice = sql.is_aservice(chat_id)
+            if not is_aservice:
+                sql.set_aservice(chat_id)
+            await message.reply_text("Enabled AntiService System. I will Delete Service Messages from Now on.")
+        elif state == "off":
+            is_aservice = sql.is_aservice(chat_id)
+            if not is_aservice:
+                await message.reply_text("AntiService System is already disabled.")
+                return ""
+            else:
+                sql.rem_aservice(chat_id)
+            await message.reply_text("Disabled AntiService System. I won't Be Deleting Service Message from Now on.")
         else:
-            sql.rem_aservice(chat_id)
-        await message.reply_text("Disabled AntiService System. I won't Be Deleting Service Message from Now on.")
-    else:
-        await message.reply_text(usage)
+            await message.reply_text(usage)
+    except Exception as e:
+        return print("aservice - " + str(e))
 
 @pbot.on_message(filters.service & filters.group)
 async def del_service(_, message):
