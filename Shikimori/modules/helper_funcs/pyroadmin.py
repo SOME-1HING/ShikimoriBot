2c123c5b41a806618a8c5f1449bdf54a68dd8ad7
SOME1HING <yashprakash2005@gmail.com>
2022-07-16 15:48:19 +0530

    Update pyroadmin.py

--- Shikimori/modules/helper_funcs/pyroadmin.py
+++ Shikimori/modules/helper_funcs/pyroadmin.py
@@ -12,11 +12,27 @@ async def is_pyroadmin(_, message):
         return False
 
 
-@pbot.on_message(filters.command("check"))
-async def check(_, message):
-    if is_pyroadmin:
-        return await message.reply_text("yes")
-    elif not is_pyroadmin:
-        return await message.rely_text("no")
-    else:
-        return await message.reply_text("error")
+@pbot.on_message(filters.command("checkl"))
+async def checkl(_, message):
+    try:
+        if is_pyroadmin:
+            return await message.reply_text("yes")
+        elif not is_pyroadmin:
+            return await message.rely_text("no")
+        else:
+            return await message.reply_text("error")
+    except Exception as e:
+        print(e)
+
+@pbot.on_message(filters.command("checkw"))
+async def checkw(_, message):
+    user = await message.user
+    try:
+        if user.is_admin:
+            return await message.reply_text("yes")
+        elif not user.is_admin:
+            return await message.rely_text("no")
+        else:
+            return await message.reply_text("error")
+    except Exception as e:
+        print(e)
