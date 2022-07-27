b88b72840b49b7f665f554879d19c3dc9858c104
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-27 09:49:45 -0700

    Update weather.py

--- Shikimori/modules/weather.py
+++ Shikimori/modules/weather.py
@@ -19,7 +19,7 @@ import time
 
 import requests
 from pytz import country_names as cname
-from telegram import ParseMode
+from telegram.constants import ParseMode
 from telegram.error import BadRequest
 
 from Shikimori import dispatcher, API_WEATHER as APPID
