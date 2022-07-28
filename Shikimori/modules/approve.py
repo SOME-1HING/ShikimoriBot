e4db99ba4773b02dcbd53ee57e912de5b239a04d
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-27 23:48:42 -0700

    Update approve.py

--- Shikimori/modules/approve.py
+++ Shikimori/modules/approve.py
@@ -42,7 +42,8 @@ from telegram.ext import CallbackContext, CallbackQueryHandler
 import Shikimori.modules.sql.approve_sql as sql
 from Shikimori.modules.helper_funcs.chat_status import user_admin
 from Shikimori.modules.log_channel import loggable
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
+from telegram.constants import ParseMode
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
 
