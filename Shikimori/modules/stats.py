20b80c2179546cc45ab56dca1888b28ddf3a5f5d
Alone神 <110736685+Alonebots@users.noreply.github.com>
2022-08-07 16:21:40 +0300

    Update stats.py

--- Shikimori/modules/stats.py
+++ Shikimori/modules/stats.py
@@ -102,8 +102,8 @@ def stats(update, context):
                     [
                         [                  
                             InlineKeyboardButton(
-                                    text="REPO",
-                                    url="https://github.com/SOME-1HING/ShikimoriBot"),
+                                    text="Owner",
+                                    url="https://github.com/Alonebots"),
                         ]
                     ]
                 ),
@@ -118,8 +118,8 @@ def stats(update, context):
                     [
                         [                  
                             InlineKeyboardButton(
-                                    text="REPO",
-                                    url="https://github.com/SOME-1HING/ShikimoriBot"),
+                                    text="Owner",
+                                    url="https://github.com/Alonebots"),
                         ]
                     ]
                 ),
