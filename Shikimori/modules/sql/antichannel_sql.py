03662a64773e45396a7fc9e91ad1079c105ce91a
SOME1HING <yashprakash2005@gmail.com>
2022-08-16 03:44:35 +0530

    Update antichannel_sql.py

--- Shikimori/modules/sql/antichannel_sql.py
+++ Shikimori/modules/sql/antichannel_sql.py
@@ -24,69 +24,24 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import threading
+from Shikimori.mongo import db
 
-from sqlalchemy import Boolean
-from sqlalchemy.sql.sqltypes import String
-from sqlalchemy import Column
+antichanneldb = db.antichannel
 
-from Shikimori.modules.sql import BASE, SESSION
+async def antichannel_status(chat_id: int) -> bool:
+    chat = antichanneldb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
 
+def enable_antichannel(chat_id):
+    is_achannel = is_achannel(chat_id)
+    if not is_achannel:
+        return
+    return antichanneldb.insert_one({"chat_id": chat_id})
 
-class AntiChannelSettings(BASE):
-    __tablename__ = "anti_channel_settings"
-    
-    chat_id = Column(String(14), primary_key=True)
-    setting = Column(Boolean, default=False, nullable=False)
-
-    def __init__(self, chat_id: int, disabled: bool):
-        self.chat_id = str(chat_id)
-        self.setting = disabled
-
-    def __repr__(self):
-        return "<Antiflood setting {} ({})>".format(self.chat_id, self.setting)
-
-
-AntiChannelSettings.__table__.create(checkfirst=True)
-ANTICHANNEL_SETTING_LOCK = threading.RLock()
-
-def enable_antichannel(chat_id: int):
-    with ANTICHANNEL_SETTING_LOCK:
-        chat = SESSION.query(AntiChannelSettings).get(str(chat_id))
-        if not chat:
-            chat = AntiChannelSettings(str(chat_id), True)
-
-        chat.setting = True
-        SESSION.add(chat)
-        SESSION.commit()
-
-
-def disable_antichannel(chat_id: int):
-    with ANTICHANNEL_SETTING_LOCK:
-        chat = SESSION.query(AntiChannelSettings).get(str(chat_id))
-        if not chat:
-            chat = AntiChannelSettings(str(chat_id), False)
-
-        chat.setting = False
-        SESSION.add(chat)
-        SESSION.commit()
-
-
-def antichannel_status(chat_id: int) -> bool:
-    with ANTICHANNEL_SETTING_LOCK:
-        d = SESSION.query(AntiChannelSettings).get(str(chat_id))
-        if not d:
-            return False
-        return d.setting
-
-
-
-
-def migrate_chat(old_chat_id, new_chat_id):
-    with ANTICHANNEL_SETTING_LOCK:
-        chat = SESSION.query(AntiChannelSettings).get(str(old_chat_id))
-        if chat:
-            chat.chat_id = new_chat_id
-            SESSION.add(chat)
-
-        SESSION.commit()
+def disable_antichannel(chat_id):
+    is_achannel = is_achannel(chat_id)
+    if is_achannel:
+        return
+    return antichanneldb.delete_one({"chat_id": chat_id})
