cb9a815068fca73a680fb894e51ca6657eb429c4
SOME1HING <yashprakash2005@gmail.com>
2022-08-16 15:25:53 +0530

    Update shippering.py

--- Shikimori/modules/shippering.py
+++ Shikimori/modules/shippering.py
@@ -57,8 +57,6 @@ tomorrow = str(dt_tom())
 
 
 @app.on_message(filters.command("couples"))
-
-@capture_err
 async def couple(_, message):
     if message.chat.type == "private":
         await message.reply_text("This command only works in groups.")
