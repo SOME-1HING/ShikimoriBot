8e5b8ba2f0310a08cfe2cc4d6b66536398cc5f58
SOME1HING <yashprakash2005@gmail.com>
2022-08-17 18:02:54 +0530

    Update pyro_admin.py

--- Shikimori/modules/helper_funcs/pyro_admin.py
+++ Shikimori/modules/helper_funcs/pyro_admin.py
@@ -13,7 +13,7 @@ from Shikimori import pbot
 ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
 THREAD_LOCK = RLock()
 
-def is_user_pyro_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
+async def is_user_pyro_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
         or user_id in DRAGONS
@@ -24,11 +24,11 @@ def is_user_pyro_admin(chat: Chat, user_id: int, member: ChatMember = None) -> b
     if not member:
         with THREAD_LOCK:
             try:
-                return user_id in ADMIN_CACHE[chat.id]
+                return await user_id in ADMIN_CACHE[chat.id]
             except KeyError:
-                chat_admins = pbot.get_chat_members(chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS)
-                admin_list = [x.user.id for x in chat_admins]
+                chat_admins = await pbot.get_chat_members(chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS)
+                admin_list = [x.user.id async for x in chat_admins]
                 ADMIN_CACHE[chat.id] = admin_list
                 return user_id in admin_list
     else:
-        return member.status in ("administrator", "creator")
+        return await member.status in ("administrator", "creator")
