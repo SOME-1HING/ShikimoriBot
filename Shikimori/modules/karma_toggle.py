5121842574926bc541f0d2275eab97700fe3d3e1
SOME1HING <yashprakash2005@gmail.com>
2022-08-17 16:26:08 +0530

    Update karma_toggle.py

--- Shikimori/modules/karma_toggle.py
+++ Shikimori/modules/karma_toggle.py
@@ -25,14 +25,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
-import re
 import Shikimori.modules.sql.karma_sql as ksql
 from Shikimori.modules.sql import log_channel_sql as logsql
 import html
 from telegram import ParseMode
-from telegram import (CallbackQuery, Chat, InlineKeyboardButton,
-                      InlineKeyboardMarkup, ParseMode, Update, User)
+from telegram import (InlineKeyboardButton,
+                      InlineKeyboardMarkup, ParseMode, Update)
 from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler)
 from telegram.utils.helpers import mention_html
 
@@ -40,102 +38,103 @@ from Shikimori.modules.helper_funcs.chat_status import user_admin, user_admin_no
 from Shikimori import  dispatcher
 from Shikimori.modules.log_channel import loggable
 
-
+bot_name = f"{dispatcher.bot.first_name}"
 
 @user_admin_no_reply
 @loggable
-def rem_karma(update: Update, context: CallbackContext) -> str:
-    query = update.callback_query
-    user = update.effective_user
+def karma_status(update: Update, context: CallbackContext):
+    query= update.callback_query
     bot = context.bot
-    match = re.match(r"rem_karma\((.+?)\)", query.data)
-    if match:
-        user_id = match.group(1)
+    user = update.effective_user
+    if query.data == "add_karma":
         chat = update.effective_chat
-        is_kuki = ksql.rem_karma(chat.id)
-        if is_kuki:
-            is_kuki = ksql.rem_karma(user_id)
+        is_chatbot = ksql.is_karma(chat.id)
+        if not is_chatbot:
+            is_chatbot = ksql.set_karma(chat.id)
             LOG = (
                 f"<b>{html.escape(chat.title)}:</b>\n"
-                f"KARMA_DISABLED\n"
+                f"KARMA_ENABLE\n"
                 f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
             log_channel = logsql.get_chat_log_channel(chat.id)
             if log_channel:
-                return bot.send_message(
+                bot.send_message(
                 log_channel,
                 LOG,
                 parse_mode=ParseMode.HTML,
                 disable_web_page_preview=True,
             )
-            return
-        else:
             update.effective_message.edit_text(
-                f"Disabled Karma System.",
+                f"{bot_name} Karma System Enabled by {mention_html(user.id, user.first_name)}.",
                 parse_mode=ParseMode.HTML,
             )
-
-    return ""
-
-
-@user_admin_no_reply
-@loggable
-def add_karma(update: Update, context: CallbackContext) -> str:
-    query= update.callback_query
-    user = update.effective_user
-    bot = context.bot
-    match = re.match(r"add_karma\((.+?)\)", query.data)
-    if match:
-        user_id = match.group(1)
+            return
+        elif is_chatbot:
+            return update.effective_message.edit_text(
+                f"{bot_name} Karma System Already Enabled.",
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            return update.effective_message.edit_text(
+                "Error!",
+                parse_mode=ParseMode.HTML,
+            )
+    elif query.data == "rem_karma":
         chat = update.effective_chat
-        is_kuki = ksql.set_karma(chat.id)
-        if is_kuki:
-            is_kuki = ksql.set_karma(user_id)
-            LOG =f"""
-<b>{html.escape(chat.title)}:</b>\n"
-KARMA_ENABLE\n"
-<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"""
+        is_chatbot = ksql.is_karma(chat.id)
+        if is_chatbot:
+            is_chatbot = ksql.rem_karma(chat.id)
+            LOG = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"KARMA_DISABLE\n"
+                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
             log_channel = logsql.get_chat_log_channel(chat.id)
             if log_channel:
-                return bot.send_message(
-                log_channel.id,
+                bot.send_message(
+                log_channel,
                 LOG,
                 parse_mode=ParseMode.HTML,
                 disable_web_page_preview=True,
             )
-            return
-        else:
             update.effective_message.edit_text(
-                f"Enabled Karma System.",
+                f"{bot_name} Karma System disabled by {mention_html(user.id, user.first_name)}.",
+                parse_mode=ParseMode.HTML,
+            )
+            return
+        elif not is_chatbot:
+            return update.effective_message.edit_text(
+                f"{bot_name} Karma System Already Disabled.",
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            return update.effective_message.edit_text(
+                "Error!",
                 parse_mode=ParseMode.HTML,
             )
 
-    return ""
-
-
 @user_admin
 @loggable
-def karma_status(update: Update, context: CallbackContext):
-    user = update.effective_user
+def karma_toggle(update: Update, context: CallbackContext):
     message = update.effective_message
     msg = "Choose an option"
     keyboard = InlineKeyboardMarkup([[
         InlineKeyboardButton(
             text="Enable",
-            callback_data="add_karma({})")],
+            callback_data=r"add_karma")],
        [
         InlineKeyboardButton(
             text="Disable",
-            callback_data="rem_karma({})")]])
+            callback_data=r"rem_karma")]])
     message.reply_text(
         msg,
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
 
-KARMA_STATUS_HANDLER = CommandHandler("karma", karma_status, run_async = True)
-ADD_KARMA_HANDLER = CallbackQueryHandler(add_karma, pattern=r"add_karma", run_async = True)
-RM_KARMA_HANDLER = CallbackQueryHandler(rem_karma, pattern=r"rem_karma", run_async = True)
+KARMA_STATUS_HANDLER = CommandHandler("karma", karma_toggle, run_async = True)
+ADD_KARMA_HANDLER = CallbackQueryHandler(karma_status, pattern=r"add_karma", run_async = True)
+RM_KARMA_HANDLER = CallbackQueryHandler(karma_status, pattern=r"rem_karma", run_async = True)
 
 dispatcher.add_handler(ADD_KARMA_HANDLER)
 dispatcher.add_handler(KARMA_STATUS_HANDLER)
