5a3c2d514e8b4255e4b793d2d920c8ad807524ee
SOME1HING <yashprakash2005@gmail.com>
2022-08-17 14:46:35 +0530

    Update chatbot_sql.py

--- Shikimori/modules/sql/chatbot_sql.py
+++ Shikimori/modules/sql/chatbot_sql.py
@@ -26,22 +26,22 @@ from Shikimori.mongo import db
 chatbotdb = db.chatbot
 
 def is_chatbot(chat_id: int):
-    chatbot = chatbotdb.find_one({"id_chat": chat_id})
+    chatbot = chatbotdb.find_one({"chat_id": chat_id})
     if chatbot:
         return True
     else:
         return False
 
-def add_chatbot(chat_id):
+def add_chatbot(chat_id: int):
     chatbot = is_chatbot(chat_id)
     if chatbot:
         return
     else:
-        return chatbotdb.insert_one({"id_chat": chat_id})
+        return chatbotdb.insert_one({"chat_id": chat_id})
 
-def rm_chatbot(chat_id):
+def rm_chatbot(chat_id: int):
     chatbot = is_chatbot(chat_id)
     if not chatbot:
         return
     else:
-        return chatbotdb.delete_one({"id_chat": chat_id})
+        return chatbotdb.delete_one({"chat_id": chat_id})
