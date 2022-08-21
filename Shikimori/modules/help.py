618474b603eb170f98221207dea7af7d623349ac
Alone神 <110736685+Alonebots@users.noreply.github.com>
2022-08-21 22:56:35 +0300

    Update help.py

--- Shikimori/modules/help.py
+++ Shikimori/modules/help.py
@@ -30,7 +30,10 @@ from Shikimori.__main__ import HELPABLE
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 
 HELP_STRINGS = """
-Click on the button bellow to get description about specifics command."""
+Gᴏᴊᴏ神 Here with
+AI Chatbot, Anime, Music, Notes, Filters, NSFW and more!
+ All commands can either be used with / or !.
+ Reach out for support: @gojo_support Oᴡɴᴇʀ: @Mr_nack_nack ."""
 
 def send_help(chat_id, text, keyboard=None):
     if not keyboard:
